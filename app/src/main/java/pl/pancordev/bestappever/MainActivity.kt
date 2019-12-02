cdfada46d3405ece9f97fb74502d959eea46d3e3
Pablo <pancordev@gmail.com>
2019-12-02 14:26:41 +0100

    Show text from inputText.

--- app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
+++ app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
@@ -1,12 +1,27 @@
 package pl.pancordev.bestappever
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide
+            .with(this)
+            .load(R.drawable.ic_warning_black_24dp)
+            .into(error_image)
+
+        submit.setOnClickListener {
+            error_description.text = find_location.text
+            Log.e("MainActivity", find_location.text.toString())
+            Toast.makeText(applicationContext, find_location.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
