f8d82e81cb84dcb11cfd80741388ee28cbd4eb19
Pablo <pancordev@gmail.com>
2019-12-02 14:56:12 +0100

    Added LocationsActivity.

---
+++ app/src/main/java/pl/pancordev/bestappever/LocationsActivity.kt
@@ -0,0 +1,41 @@
+package pl.pancordev.bestappever
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class LocationsActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_locations)
+        Log.e(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
+    }
+}
--- app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
+++ app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
@@ -1,27 +1,62 @@
 package pl.pancordev.bestappever
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.e(TAG, "onCreate")
         Glide
             .with(this)
             .load(R.drawable.ic_warning_black_24dp)
             .into(error_image)
 
         submit.setOnClickListener {
-            error_description.text = find_location.text
-            Log.e("MainActivity", find_location.text.toString())
-            Toast.makeText(applicationContext, find_location.text, Toast.LENGTH_SHORT).show()
+//            error_description.text = find_location.text
+//            Log.e(TAG, find_location.text.toString())
+//            Toast.makeText(applicationContext, find_location.text, Toast.LENGTH_SHORT).show()
+            val locationsActivityIntent = Intent(this, LocationsActivity::class.java)
+            startActivity(locationsActivityIntent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
     }
 }
