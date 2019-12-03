81a6a0f6b110ad515012e6d27fe379be7d4ed4cb
Pablo <pancordev@gmail.com>
2019-12-03 10:59:49 +0100

    Added support of toolbar for coming back.

--- app/src/main/java/pl/pancordev/bestappever/ThirdActivity.kt
+++ app/src/main/java/pl/pancordev/bestappever/ThirdActivity.kt
@@ -3,20 +3,31 @@ package pl.pancordev.bestappever
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_third.*
 import kotlinx.android.synthetic.main.activity_third.toolbar
+import java.lang.IllegalStateException
 
 class ThirdActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) ?: throw IllegalStateException("No toolbar to set")
 
         button.setOnClickListener {
             val mainActivityIntent = Intent(this, MainActivity::class.java)
             mainActivityIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(mainActivityIntent)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val mainActivityIntent = Intent(this, MainActivity::class.java)
+        mainActivityIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(mainActivityIntent)
+        return super.onOptionsItemSelected(item)
     }
 }
