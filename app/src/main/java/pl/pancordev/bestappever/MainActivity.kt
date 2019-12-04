baeaa4eb795067380f5f4d426f05a0c151b087c3
Pablo <pancordev@gmail.com>
2019-12-04 14:14:00 +0100

    Added PlacesActivity.

--- app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
+++ app/src/main/java/pl/pancordev/bestappever/MainActivity.kt
@@ -1,11 +1,13 @@
 package pl.pancordev.bestappever
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.pancordev.bestappever.locations.LocationsActivity
+import pl.pancordev.bestappever.places.PlacesActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +32,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, find_location.text, Toast.LENGTH_SHORT).show()
             val locationsActivityIntent = LocationsActivity.sendAdditionalText(this, find_location.text.toString())
             startActivity(locationsActivityIntent)
+        }
+
+        places_button.setOnClickListener {
+            startActivity(Intent(this, PlacesActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/pl/pancordev/bestappever/places/PlacesActivity.kt
@@ -0,0 +1,16 @@
+package pl.pancordev.bestappever.places
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.pancordev.bestappever.R
+import pl.pancordev.bestappever.locations.LocationsActivity
+
+class PlacesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_places)
+    }
+}
